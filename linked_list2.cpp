c12697c418e0a04082e17ccb9e4de84f73f1a87a
Maria Emreen Juarez Viray <emreenviray@gmail.com>
2016-12-17 11:34:11 +0800

    Update linked_list2.cpp

--- linked_list2.cpp
+++ linked_list2.cpp
@@ -16,13 +16,14 @@ public:
     void addValue(int val){
         Node *n = new Node();   
         n->x = val;             
-        n->next = head;                         
+        n->next = head;               
         head = n;              
     }
 
     int popValue(){
         Node *n = head;
         int ret;
+        
 		if (head==NULL){
 			return NULL;
 		}
@@ -32,12 +33,30 @@ public:
 			delete n;
         	return ret;
 		   }
+   		
     }
+    
     void menu(){
     	cout<<"MAIN MENU\n";
     	cout<<"1 - Insert into linked list\n";
     	cout<<"2 - Pop\n";
     	cout<<"3 - Exit\n";
+    	cout<<"4 - Display\n";
+	}
+	
+	void display(){
+		Node *n;
+		n = head;
+		if (n == NULL){
+			cout<<"\nNothing to display. Please add a value first.";
+		}
+		else{
+			while(n!=NULL){
+				cout<< n->x <<" ";
+				n = n->next;
+			}	
+		}
+		getch();
 	}
 
 private:
@@ -53,6 +72,7 @@ int main() {
 		list.menu();
 		cout<<"---\nEnter choice: ";
 		cin>>choice;
+		
 		switch(choice){
 			case 1: {
 				cout<<"\nEnter Number: ";
@@ -62,16 +82,22 @@ int main() {
 			}
 			case 2: {
 				n = list.popValue();
+				
 				if (n==NULL){
 					cout<<"\nNothing to pop. Add value first."; 
 				}
 				else{
 					cout<<"\n"<< n <<" has been deleted.";
 				}
+
 				getch();
 				break;
 			}
 			case 3: exit(1);
+			case 4:{
+				list.display();
+				break;
+			}
 			default: break;
 		}
 	}
