4d1b8cbb3d04e6ecf0621928ef9de9d0e89a92f1
Maria Emreen Juarez Viray <emreenviray@gmail.com>
2016-12-17 11:06:27 +0800

    Create linked_list2.cpp

---
+++ linked_list2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+
+class LinkedList{
+    struct Node {
+        int x;
+        Node *next;
+    };
+
+public:
+    LinkedList(){
+        head = NULL; // set head to NULL
+    }
+
+    void addValue(int val){
+        Node *n = new Node();   
+        n->x = val;             
+        n->next = head;         
+                               
+        head = n;              
+    }
+
+    int popValue(){
+        Node *n = head;
+        int ret;
+        
+		if (head==NULL){
+			return -1;
+		}
+		else{
+			ret = n->x;
+			head = head->next;
+			delete n;
+        	return ret;
+		   }
+   		
+    }
+    
+    void menu(){
+    	cout<<"MAIN MENU\n";
+    	cout<<"1 - Insert into linked list\n";
+    	cout<<"2 - Pop\n";
+    	cout<<"3 - Exit\n";
+	}
+
+private:
+    Node *head; 
+};
+
+int main() {
+    LinkedList list;
+	int n, choice;
+	
+	while(1){
+		system("cls");
+		list.menu();
+		cout<<"---\nEnter choice: ";
+		cin>>choice;
+		
+		switch(choice){
+			case 1: {
+				cout<<"\nEnter Number: ";
+				cin>>n;
+				list.addValue(n);
+				break;
+			}
+			case 2: {
+				n = list.popValue();
+				
+				if (n==-1){
+					cout<<"\nNothing to pop. Add value first."; 
+				}
+				else{
+					cout<<"\n"<< n <<" has been deleted.";
+				}
+
+				getch();
+				break;
+			}
+			case 3: exit(1);
+			default: break;
+		}
+	}
+    return 0;
+}
